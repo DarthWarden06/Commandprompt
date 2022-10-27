d15951af4fda0e0aad45c9195540ddd7ba3645c2
DarthWarden06 <114066074+DarthWarden06@users.noreply.github.com>
2022-10-27 16:27:17 +0200

    First Option

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 
+
 var input = document.getElementById("commandline");
 input.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    document.getElementById("commandline").click();
-  }
-});
-
-// var clear = document.getElementById("commandline");
-input.addEventListener("keypress", function(event) {
-  if (event.key === "-") {
-    event.preventDefault();
-    document.getElementById("clear").click();
     document.getElementById("commandline").click();
   }
 });
@@ -35,11 +27,20 @@ function showVal() {
     document.getElementById("clear").click()
     if(tmp =="/!"){
       document.getElementById("clear").click()
-      document.getElementById('console_content').innerHTML += ("(/tp1) Ført videre til webiste" + "<br/>" + "(/tp2) Ført videre til project 2" + "<br/>" + "(/clear) eller '-' for at cleare chatten." + "<br/>")
+      document.getElementById('console_content').innerHTML += ("(/tp1) Ført videre til webiste" + "<br/>" + "(/tp2) Ført videre til project 2" + "<br/>" + "(/clear) eller '-' for at cleare chatten" + "<br/>" + "(/run) for at køre et program")
     }
+  
   }
- 
+  if(tmp == "/run"){
+    // console.log("hej")
+    window.open("nyside.html")
+    setTimeout(hackerMan, 200)
+   
+  }
+  
+
 }
+
 function clearMe(){
   // console.log("clearMe")
   const textarea = document.getElementById('console_content').innerHTML ='';
@@ -51,3 +52,28 @@ function commandList(){
   document.getElementById("commandline").click()
 
 }
+function hackerMan(){
+  document.getElementById("clear").click()
+  document.getElementById("console_content").innerHTML += ("Victor" + "<br/>" + "Jeg" + "<br/>" + "hacker" + "<br/>" + "alle" + "<br/>")
+}
+function allowClear(){
+  var myCheck = document.getElementById("myCheck")
+  
+  if (myCheck.checked == true){
+    document.getElementById('console_content').innerHTML += ("<br/>" + "Clear is enabled" + "<br/>")
+    input.addEventListener("keypress", function(event) { 
+      if (event.key === "-") {
+        event.preventDefault();
+        document.getElementById("clear").click();
+        document.getElementById("commandline").click();
+        }
+   
+    }
+    )
+
+ }
+
+if (myCheck.checked == false){
+  document.getElementById('console_content').innerHTML += ("<br/>" + "Clear is disabled" + "<br/>")
+}
+} 
